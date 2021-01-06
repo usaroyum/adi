e61f5eca3a1071148770161f22ce8fbb2a3741c2
usaroyum <75182777+usaroyum@users.noreply.github.com>
2021-01-07 04:37:16 +0700

    Add files via upload

---
+++ Portofolio/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portofolio</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="index.js"></script>
+</head>
+<body>
+
+<nav class="navigasi">
+      <div><a class="active" href="">About</a></div>
+      <div><a href="#skills">Skills</a></div>
+      <div><a href="#product">Product</a></div>
+      <div class="contact-nav"><a href="#contact"><span>Contact</span></a></div>
+    </nav>
+   
+
+    <div class="container">
+        <section class="about" id="about">
+            <h1><span>Hi</span>, my name is Adi</h1>
+            <p>I have exprience about <span>UI / UX design, Graphic design, 
+                Web developer</span> & <span>Editor VFX</span>. Who have great abilites 
+                can always work anytime and anywhere.
+                I can communicate very well and interested in solving new problems.</p>
+        </section>
+
+         <section class="skills" id="skills" name="skills">
+             <h1><span>Experienced</span> using</h1>
+             <img src="img/skills.svg" alt="">
+           
+         
+
+        </section>
+
+        <section class="product" id="product">
+            <h1>Own <span>Product</span></h1>
+                <div class="category">
+                    <div class="category-child">
+                        <img src="img/computer.svg" alt="">
+                        <h3>UI / UX Design</h3>
+                        <button>Detail</button>
+                    </div>
+                    <div class="category-child">
+                        <img src="img/programming.svg" alt="">
+                        <h3>Application</h3>
+                        <button>Detail</button>
+                    </div>
+                    <div class="category-child">
+                        <img src="img/computer.svg" alt="">
+                        <h3>Logo Design</h3>
+                        <button>Detail</button>
+                    </div>
+                    <div class="category-child">
+                        <img src="img/computer.svg" alt="">
+                        <h3>Illustration</h3>
+                        <button>Detail</button>
+                    </div>
+                </div>
+        
+            
+
+        </section>
+
+        <section class="contact" id="contact">
+            <h1>Contact</h1>
+            <div class="person">
+            <img src="img/email.svg" alt=""> <p class="email">usaroyum@gmail.com</p>
+            </div>
+           
+            <p>If you need someone to help you create  amazing project. Contact me via email or <a href="">Whatsapp</a></p>
+        </section>
+       
+    </div>
+
+
+
+</body>
+</html>
